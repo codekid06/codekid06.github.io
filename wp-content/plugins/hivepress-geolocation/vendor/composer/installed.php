e59fe63ee856cd8549e50dbe9d51f71a63bcd48d
codekid06 <bishtyash689@gmail.com>
2022-08-21 14:46:02 +0530

    done

---
+++ wp-content/plugins/hivepress-geolocation/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.3.5',
+        'version' => '1.3.5.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '69daae301d821d122776efc747dd3aa6f94b4458',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.3.5',
+            'version' => '1.3.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '69daae301d821d122776efc747dd3aa6f94b4458',
+            'dev_requirement' => false,
+        ),
+    ),
+);
